fcbaf4e0f658379aff9e50ba99b274e2f0b982da
maximdervis <maximdervis@gmail.com>
2019-10-30 17:47:59 +0300

    some files have been added

---
+++ script.js
@@ -0,0 +1,13 @@
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const currenBg = document.querySelector("h3");
+const body = document.querySelector("body");
+
+var changeColor = () => 
+{
+	body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+	currenBg.textContent = body.style.background + `;`;
+}
+
+color1.addEventListener("change", changeColor);
+color2.addEventListener("change", changeColor);
